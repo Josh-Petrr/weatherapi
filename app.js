3a697a94af415c5ef6a0594e878d146b6ab1a71f
JOSH PETER <155379896+Josh-Petrr@users.noreply.github.com>
2025-11-24 12:45:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function searchCity() {
     const city = document.getElementById("city").value;
-    const API_KEY = "8f3d2cdc837457e367cf7b9afdc07084";  //Replace
+    const API_KEY = "620b0a4d1ab33f6584aede67108367dd";  //Replace
 
     document.getElementById("output").innerText = "Loading...";
 
